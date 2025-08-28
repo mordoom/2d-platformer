04925e20251aeb837bbb92d61acfaf0c1c57033d
Alex Mordue <alex.mordue@hotmail.com.au>
2025-08-28 16:09:47 +1000

    remove message since button prompt is probably enough

--- world/cave/levels/room_with_campfire.gd
+++ world/cave/levels/room_with_campfire.gd
@@ -1,7 +1,1 @@
 extends Level
-
-func _on_message_trigger_body_entered(body: Node2D) -> void:
-	SignalBus.emit_signal("on_display_message", "Interact with campfires to restore health")
-
-func _on_message_trigger_body_exited(body: Node2D) -> void:
-	SignalBus.emit_signal("on_remove_message")
