fe489171448a87da5ff344f9dc2908c9a7edc004
Alex Mordue <alex.mordue@hotmail.com.au>
2025-09-04 09:50:23 +1000

    formatting

--- characters/states/hit_state.gd
+++ characters/states/hit_state.gd
@@ -11,24 +11,27 @@ var hit_stop_duration = 0.2
 var hit_direction
 
 func _ready() -> void:
-	damageable.connect("on_hit", on_damageable_hit)
+    damageable.connect("on_hit", on_damageable_hit)
+
+func on_exit():
+    character.velocity.x = 0
 
 func on_damageable_hit(_node: Node, _amount: int, direction: Vector2):
-	hit_direction = direction
+    hit_direction = direction
 
-	# TODO: find a better way to do knockback
-	character.velocity += knockback_speed * direction
+    # TODO: find a better way to do knockback
+    character.velocity = knockback_speed * direction
 
-	if (damageable.is_dead()):
-		emit_change_state("dead")
-	else:
-		emit_change_state("hit")
-		playback.travel("hit")
+    if (damageable.is_dead()):
+        emit_change_state("dead")
+    else:
+        emit_change_state("hit")
+        playback.travel("hit")
 
-	Engine.time_scale = hit_stop_time_scale
-	await get_tree().create_timer(hit_stop_duration * hit_stop_time_scale).timeout
-	Engine.time_scale = 1
+    Engine.time_scale = hit_stop_time_scale
+    await get_tree().create_timer(hit_stop_duration * hit_stop_time_scale).timeout
+    Engine.time_scale = 1
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
-	if (anim_name == "hit" && get_parent().current_state == self):
-		emit_reset_state()
+    if (anim_name == "hit" && get_parent().current_state == self):
+        emit_reset_state()
