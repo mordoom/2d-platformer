11d09cbbbe0f3111b48ca962caaef40cf79abf3f
Alex Mordue <alex.mordue@hotmail.com.au>
2025-09-04 11:50:20 +1000

    create button prompt scene

--- components/button_prompt_component.gd
+++ components/button_prompt_component.gd
@@ -2,13 +2,14 @@ extends Node
 
 class_name ButtonPromptComponent
 
-@onready var button_prompt: TextureRect = get_parent().get_node("Label")
+@onready var button_prompt: TextureRect = $Label
 
 @export var action = "interact"
 
 func _ready() -> void:
 	button_prompt.visible = false
-	button_prompt.texture.path = action
+	if button_prompt.texture and button_prompt.texture.has_method("set_path"):
+		button_prompt.texture.path = action
 
 func show_prompt() -> void:
 	button_prompt.visible = true
