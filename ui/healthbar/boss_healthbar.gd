e108ba3341c701d9dfc75e8ce1593d9dbfa028e5
Alex Mordue <alex.mordue@hotmail.com.au>
2025-09-16 09:13:09 +1000

    upgrade to godot 4.5

--- ui/healthbar/boss_healthbar.gd
+++ ui/healthbar/boss_healthbar.gd
@@ -1,33 +1,35 @@
 class_name BossHealthbar
 extends CanvasLayer
 
-@export var state_machine: CharacterStateMachine
+@export var hurtbox: Hurtbox
+@export var health_component: HealthComponent
 @export var boss_name: String
-@export var damageable: DamageableComponent
 
 @onready var healthbar: ProgressBar = $Healthbar
 @onready var label: Label = $Label
 @onready var healthbar_timer: Timer = $HealthbarTimer
 
 func _ready() -> void:
-	label.text = boss_name
-	visible = false
-	healthbar.init_health(damageable.health)
-	healthbar_timer.timeout.connect(show_hide_healthbar)
-	damageable.connect("on_hit", on_damageable_hit)
+    label.text = boss_name
+    visible = false
+    healthbar.init_health(health_component.health)
+    healthbar_timer.timeout.connect(show_healthbar)
+    hurtbox.connect("on_hit", on_hit)
+    health_component.connect("dead", dead)
 
 func init_health(new_health: int) -> void:
-	healthbar.init_health(new_health)
+    healthbar.init_health(new_health)
 
-func on_damageable_hit(_node: Node, _damage_taken: int, _direction: Vector2) -> void:
-	healthbar.health = damageable.health
+func on_hit(damage: int, _knockback_velocity: float, _direction: Vector2, stun: bool) -> void:
+    healthbar.health = health_component.health
 
 func dead() -> void:
-	if healthbar_timer.is_stopped() && visible:
-		healthbar_timer.start()
+    if healthbar_timer.is_stopped() && visible:
+        healthbar_timer.start()
+        healthbar_timer.timeout.connect(hide_healthbar)
 
-func show_hide_healthbar() -> void:
-	if state_machine.is_dead():
-		visible = false
-	else:
-		visible = true
+func show_healthbar() -> void:
+    visible = true
+
+func hide_healthbar() -> void:
+    visible = false
