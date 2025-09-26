01ebeeba0e6641c789812d1c331f26df3d07add0
Alex Mordue <alex.mordue@hotmail.com.au>
2025-09-26 15:07:13 +1000

    add ability to sail on ship

--- global/signal_bus.gd
+++ global/signal_bus.gd
@@ -8,6 +8,7 @@ signal rum_consumed()
 signal on_bottle_rum_collected(area: Area2D, amount: int)
 signal on_campfire_rested(area: Area2D)
 signal money_collected(area: Area2D, amount: int)
+signal on_sloop_start(area: Area2D)
 
 signal on_display_message(text: String)
 signal on_remove_message()
--- world/beach/sloop/sloop.gd
+++ world/beach/sloop/sloop.gd
@@ -1,1 +1,15 @@
-extends Node2D
+extends AnimatableBody2D
+
+var moving := false
+var speed := 50
+
+func _ready():
+    SignalBus.connect("on_sloop_start", on_sloop_start)
+
+func _physics_process(delta):
+    if moving:
+        position.x += speed * delta
+
+
+func on_sloop_start(_area: Area2D):
+    moving = true
