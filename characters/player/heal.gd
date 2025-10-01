c13c6aab99befdf52601c824294eb4e88714b07c
Alex Mordue <alex.mordue@hotmail.com.au>
2025-10-02 09:28:36 +1000

    add burp sound when healing

--- characters/player/heal.gd
+++ characters/player/heal.gd
@@ -1,5 +1,9 @@
 extends CharacterState
 
+var time := 0.0
+var burp_time := 0.6
+@export var burp_sound: AudioStream
+
 func _ready() -> void:
     set_guard(healing_guard)
 
@@ -8,9 +12,15 @@ func healing_guard() -> bool:
 
 func _enter() -> void:
     blackboard.set_var(GameConstants.BlackboardVars.current_speed_var, 0)
+    time = 0
     super._enter()
 
-func _update(_delta: float) -> void:
+func _update(delta: float) -> void:
+    time += delta
+    if time >= burp_time:
+        time = 0
+        SoundManager.play_sound(burp_sound)
+
     if not animation_player.is_playing() or animation_player.assigned_animation != animation_name:
         SignalBus.emit_signal("rum_consumed")
         owner.set_health(owner.get_health() + 20)
