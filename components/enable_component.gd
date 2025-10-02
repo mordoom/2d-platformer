b26e22c38442ecbd2d3770be338e5a076c95ff16
Alex Mordue <alex.mordue@hotmail.com.au>
2025-10-02 14:10:42 +1000

    can sail on sloop once first mate is freed

--- components/enable_component.gd
+++ components/enable_component.gd
@@ -5,17 +5,29 @@ class_name EnableComponent
 @export var enabler_string: String
 
 func _ready():
-    if enabler_string:
-        if GameState.env_is_changed(enabler_string):
-            get_child(0).visible = !get_child(0).visible
-        else:
-            SignalBus.connect("on_env_change", _on_enable)
-    else:
-        if GameState.env_is_changed(enabler):
-            get_child(0).visible = !get_child(0).visible
-        else:
-            SignalBus.connect("on_env_change", _on_enable)
+	if enabler_string:
+		if GameState.env_is_changed(enabler_string):
+			toggle_child()
+		else:
+			SignalBus.connect("on_env_change", _on_enable)
+	else:
+		if GameState.env_is_changed(enabler):
+			toggle_child()
+		else:
+			SignalBus.connect("on_env_change", _on_enable)
+
+	var child = get_child(0)
+	if child is Area2D:
+		child.monitoring = child.visible
+		child.monitorable = child.visible
 
 func _on_enable(node: Node2D) -> void:
-    if node == enabler:
-        get_child(0).visible = !get_child(0).visible
+	if node == enabler:
+		toggle_child()
+	
+func toggle_child() -> void:
+	var child = get_child(0)
+	child.visible = !child.visible
+	if child is Area2D:
+		child.monitoring = child.visible
+		child.monitorable = child.visible
--- world/beach/sloop/sloop.gd
+++ world/beach/sloop/sloop.gd
@@ -1,7 +1,8 @@
 extends AnimatableBody2D
 
 var moving := false
-var speed := 50
+@export var speed := 50
+@export var collision_shape: StaticBody2D
 
 func _ready():
     SignalBus.connect("on_sloop_start", on_sloop_start)
@@ -11,4 +12,8 @@ func _physics_process(delta):
         position.x += speed * delta
 
 func on_sloop_start(_area: Area2D):
-    Dialogic.start_timeline("sloop repair")
+    if GameState.env_is_changed("res://world/beach/starting_beach2.tscn/root/GameManager/StartingBeach/Tnt"):
+        moving = true
+        collision_shape.set_collision_layer_value(1, false)
+    else:
+        Dialogic.start_timeline("sloop repair")
