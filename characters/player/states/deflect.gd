62241f046abde177fabfb8625eac77b234ec2e38
Alex Mordue <alex.mordue@hotmail.com.au>
2025-10-04 10:27:54 +1000

    parry cooldown

--- characters/player/states/deflect.gd
+++ characters/player/states/deflect.gd
@@ -1,5 +1,14 @@
 extends CharacterState
 
+@onready var cooldown_timer: Timer = $Timer
+
+func _ready():
+    set_guard(cooldown_check)
+
+func cooldown_check() -> bool:
+    return cooldown_timer.is_stopped()
+
 func _enter() -> void:
     super._enter()
     blackboard.set_var(GameConstants.BlackboardVars.current_speed_var, 0)
+    cooldown_timer.start()
