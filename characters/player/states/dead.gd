e60cb8b55951df55260d172846521c76dd2ded89
Alex Mordue <alex.mordue@hotmail.com.au>
2025-10-08 11:31:42 +1100

    add death sound

--- characters/player/states/dead.gd
+++ characters/player/states/dead.gd
@@ -1,9 +1,12 @@
 extends CharacterState
 
+@export var death_sound: AudioStream
+
 func _enter() -> void:
     super._enter()
     owner.hitbox.monitoring = false
     SignalBus.emit_signal("character_died", owner)
+    SoundManager.play_sound(death_sound)
 
 func _update(_delta: float) -> void:
     owner.velocity.x = move_toward(owner.velocity.x, 0, owner.movement_speed)
