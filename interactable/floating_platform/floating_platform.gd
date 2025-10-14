1ecc877c4e373f479cb519c0596e3c0c349a66cc
Alex Mordue <alex.mordue@hotmail.com.au>
2025-10-14 16:22:01 +1100

    floating platform improvements

--- interactable/floating_platform/floating_platform.gd
+++ interactable/floating_platform/floating_platform.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-@onready var start_pos: Vector2 = self.position
+@onready var start_pos: Vector2 = self.global_position
 @export var offset_pos: Vector2
 @export var duration: float = 2
 @export var pause_duration: float = 1
 @export var enabled := true
+
+@onready var animatablebody: AnimatableBody2D = $AnimatableBody2D
 
 func _ready() -> void:
     if enabled:
@@ -12,8 +14,8 @@ func _ready() -> void:
 
 func begin_moving() -> void:
     var tween: Tween = get_tree().create_tween().bind_node(self)
-    tween.tween_property(self, "position", start_pos + offset_pos, duration)
+    tween.tween_property(animatablebody, "global_position", start_pos + offset_pos, duration)
     tween.tween_interval(pause_duration)
-    tween.tween_property(self, "position", start_pos, duration)
+    tween.tween_property(animatablebody, "global_position", start_pos, duration)
     tween.tween_interval(pause_duration)
-    tween.set_loops()
+    tween.set_loops().set_parallel(false)
