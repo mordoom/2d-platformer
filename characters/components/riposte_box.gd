2ab69a76d5cc9ec2d91e699795268cc8a3a18967
Alex Mordue <alex.mordue@hotmail.com.au>
2025-10-16 14:14:30 +1100

    add ripostes to boss

--- characters/components/riposte_box.gd
+++ characters/components/riposte_box.gd
@@ -4,30 +4,30 @@ extends Area2D
 @export var posture_component: PostureComponent
 
 func _ready() -> void:
-    monitoring = false
-    posture_component.connect("posture_break", _on_posture_break)
-    connect("body_entered", _on_body_entered)
-    connect("body_exited", _on_body_exited)
+	monitoring = false
+	posture_component.connect("posture_break", _on_posture_break)
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
 
 func _on_posture_break() -> void:
-    monitoring = true
+	monitoring = true
 
 func on_posture_break_finish() -> void:
-    monitoring = false
+	monitoring = false
 
 func _on_body_entered(body: Node2D) -> void:
-    if body is Player:
-        body.in_riposte_distance()
+	if body is Player:
+		body.in_riposte_distance()
 
 func _on_body_exited(body: Node2D) -> void:
-    if body is Player:
-        body.out_riposte_distance()
+	if body is Player:
+		body.out_riposte_distance()
 
 func die() -> void:
-    on_posture_break_finish()
+	on_posture_break_finish()
 
 func flip(current_direction: float) -> void:
-    if current_direction < 0:
-        scale.x = -1
-    elif current_direction > 0:
-        scale.x = 1
+	if current_direction < 0:
+		scale.x = -1
+	elif current_direction > 0:
+		scale.x = 1
